72b2b202eab4e3cb85a0738eed57513883e72123
DenRebrov <den_rebrov@list.ru>
2019-08-10 10:57:56 +0300

    task12_omniauth completed

--- app/services/find_for_oauth.rb
+++ app/services/find_for_oauth.rb
@@ -5,25 +5,25 @@ class Services::FindForOauth
     @auth = auth
   end
 
-  # def call
-  #   authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-  #   return authorization.user if authorization
-  #
-  #   email = auth.info[:email]
-  #   user = User.where(email: email).first
-  #   if user
-  #     user.create_authorization(auth)
-  #   else
-  #     password = Devise.friendly_token[0, 20]
-  #
-  #     if auth.info[:provider] == 'github'
-  #       user = User.create!(email: email, password: password, password_confirmation: password)
-  #     else
-  #       user = User.create!(email: 'den_rebrov@mail.ru', password: password, password_confirmation: password)
-  #     end
-  #     user.create_authorization(auth)
-  #   end
-  #
-  #   user
-  # end
+  def call
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+
+      if auth.info[:provider] == 'github'
+        user = User.create!(email: email, password: password, password_confirmation: password)
+      else
+        user = User.create!(email: 'den_rebrov@mail.ru', password: password, password_confirmation: password)
+      end
+      user.create_authorization(auth)
+    end
+
+    user
+  end
 end
