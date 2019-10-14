bb93a85a618182f8dbe8a20511f3c7a2e884ed57
DenRebrov <den_rebrov@list.ru>
2019-10-14 13:02:21 +0300

    prepare

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,25 @@ class User < ApplicationRecord
   end
 
   def self.find_for_oauth(auth)
-    Services::FindForOauth.new(auth).call
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    if user
+      user.create_authorization(auth)
+    else
+      password = Devise.friendly_token[0, 20]
+
+      if auth.info[:provider] == 'github'
+        user = User.create!(email: email, password: password, password_confirmation: password)
+      else
+        user = User.create!(email: 'den_rebrov@mail.ru', password: password, password_confirmation: password)
+      end
+      user.create_authorization(auth)
+    end
+
+    user
   end
 
   def create_authorization(auth)
